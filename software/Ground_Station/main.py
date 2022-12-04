e0f3d85624160fd6a8ef6927c95b27fde32a5924
meslane <merrickslane@gmail.com>
2022-12-04 12:02:52 -0800

    start ground station code

---
+++ software/Ground_Station/main.py
@@ -0,0 +1,8 @@
+
+
+def main():
+	print("One day I will be a ground station, big and tall.")
+	print("But for now I'm just a placeholder, short and small...")
+    
+if __name__ == "__main__":
+    main()
