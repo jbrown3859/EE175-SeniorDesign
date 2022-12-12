10936b86d3ba2c648a3fc67d1b978026c4b16e96
meslane <merrickslane@gmail.com>
2022-12-12 01:53:48 -0800

    add queue monitoring functions

--- software/RF_Board/incl/backend.c
+++ software/RF_Board/incl/backend.c
@@ -4,6 +4,8 @@
 #include <util.h>
 
 #define RADIOTYPE_SBAND 1
+#define RX_FIFO_SIZE 2048
+#define RX_FIFO_PACKETS 256
 
 /* UART data structure */
 char UART_RXBUF[256];
@@ -11,13 +13,15 @@ unsigned char UART_RX_PTR = 0;
 unsigned char UART_RX_BASE = 0;
 
 /* Radio data structure*/
-char RADIO_RXBUF[2048];
+char RADIO_RXBUF[RX_FIFO_SIZE];
 unsigned int RF_RX_HEAD = 0;
 unsigned int RF_RX_BASE = 0;
 
-unsigned int RADIO_RX_PTRS[256]; //pointers to packet locations inside the RX queue
+unsigned int RADIO_RX_PTRS[RX_FIFO_PACKETS]; //pointers to packet locations inside the RX queue
 unsigned char RF_RX_PTR_HEAD = 0;
 unsigned char RF_RX_PTR_BASE = 0;
+
+char rx_buffer_flags;
 
 char timeout_flag = 0;
 
@@ -35,36 +39,76 @@ int read_UART_FIFO(void) {
 }
 
 unsigned char get_UART_FIFO_size(void) {
-    return (UART_RX_PTR - UART_RX_BASE);
+    if (UART_RX_PTR >= UART_RX_BASE) {
+        return UART_RX_PTR - UART_RX_BASE;
+    }
+    else  {
+        return (256 - UART_RX_BASE) + UART_RX_PTR;
+    }
 }
 
-/* RF Queue */
+/* RX Queue */
 void write_RX_FIFO(char* data, unsigned char len) {
     unsigned char i;
 
-    for(i=0;i<len;i++) {
-        RADIO_RXBUF[RF_RX_HEAD] = data[i];
-        RF_RX_HEAD = RF_RX_HEAD < 2047 ? RF_RX_HEAD + 1 : 0;
+    if (get_RX_FIFO_size() + len >= (RX_FIFO_SIZE-1)) { //data overflow
+        rx_buffer_flags |= 0x01;
+    }
+    else {
+        rx_buffer_flags &= ~0x01;
     }
 
-    RADIO_RX_PTRS[RF_RX_PTR_HEAD] = RF_RX_HEAD; //push pointer to queue
-    RF_RX_PTR_HEAD++;
+    if (get_RX_FIFO_packet_num() == (RX_FIFO_PACKETS-1)) { //packet overflow
+        rx_buffer_flags |= 0x02;
+    }
+    else {
+        rx_buffer_flags &= ~0x02;
+    }
+
+    if ((rx_buffer_flags & 0x03) == 0) { //if no overflows
+        for(i=0;i<len;i++) {
+            RADIO_RXBUF[RF_RX_HEAD] = data[i];
+            RF_RX_HEAD = RF_RX_HEAD < (RX_FIFO_SIZE-1) ? RF_RX_HEAD + 1 : 0;
+        }
+
+        RADIO_RX_PTRS[RF_RX_PTR_HEAD] = RF_RX_HEAD; //push pointer to queue
+        RF_RX_PTR_HEAD++;
+    }
 }
 
 void read_RX_FIFO(void) {
     unsigned int i;
     if (RF_RX_PTR_HEAD != RF_RX_PTR_BASE) { //if not empty
-        for(i=RF_RX_BASE;i!=RADIO_RX_PTRS[RF_RX_PTR_BASE];i = (i < 2047 ? i+1 : 0)) { //eat up from bottom
+        for(i=RF_RX_BASE;i!=RADIO_RX_PTRS[RF_RX_PTR_BASE];i = (i < (RX_FIFO_SIZE-1) ? i+1 : 0)) { //eat up from bottom
             putchar(RADIO_RXBUF[RF_RX_BASE]);
-            RF_RX_BASE = RF_RX_BASE < 2047 ? RF_RX_BASE + 1 : 0;
+            RF_RX_BASE = RF_RX_BASE < (RX_FIFO_SIZE-1) ? RF_RX_BASE + 1 : 0;
         }
         RF_RX_PTR_BASE++;
+    }
+}
+
+unsigned char get_RX_FIFO_packet_num(void) {
+    if (RF_RX_PTR_HEAD >= RF_RX_PTR_BASE) {
+        return RF_RX_PTR_HEAD - RF_RX_PTR_BASE;
+    }
+    else {
+        return (RX_FIFO_PACKETS - RF_RX_PTR_BASE) + RF_RX_PTR_HEAD;
+    }
+}
+
+unsigned int get_RX_FIFO_size(void) {
+    if (RF_RX_HEAD >= RF_RX_BASE) {
+        return RF_RX_HEAD - RF_RX_BASE;
+    }
+    else {
+        return (RX_FIFO_SIZE - RF_RX_BASE) + RF_RX_HEAD;
     }
 }
 
 void main_loop(void) {
     enum State state = INIT;
     char command;
+    char temp;
 
     struct RadioInfo info;
 
@@ -83,54 +127,11 @@ void main_loop(void) {
                 case 0x61: //get info
                     state = SEND_INFO;
                     break;
+                case 0x62: //send RX buffer size
+                    state = SEND_RX_BUF_STATE;
+                    break;
                 }
             }
-
-            /* test */
-            /*
-            putchars("===\n\r");
-            putchars("Writing FIFO");
-            write_RF_FIFO(dummyPacket, 16);
-
-            putchars("\n\rRX_BASE     ");
-            print_dec(RF_RX_BASE, 4);
-            putchars("\n\rRX_HEAD     ");
-            print_dec(RF_RX_HEAD, 4);
-
-            putchars("\n\rRX_PTR_BASE ");
-            print_dec(RF_RX_PTR_BASE, 4);
-            putchars("\n\rRX_PTR_HEAD ");
-            print_dec(RF_RX_PTR_HEAD, 4);
-
-            putchars("\n\rReading FIFO\n\r");
-            read_RF_FIFO();
-
-            putchars("\n\rRX_BASE     ");
-            print_dec(RF_RX_BASE, 4);
-            putchars("\n\rRX_HEAD     ");
-            print_dec(RF_RX_HEAD, 4);
-
-            putchars("\n\rRX_PTR_BASE ");
-            print_dec(RF_RX_PTR_BASE, 4);
-            putchars("\n\rRX_PTR_HEAD ");
-            print_dec(RF_RX_PTR_HEAD, 4);
-
-            putchars("\n\r===\n\r");
-            */
-            write_RX_FIFO("hello",5);
-            write_RX_FIFO("a",1);
-            write_RX_FIFO("hello world 1234", 16);
-            write_RX_FIFO("WHAT HATH GOD WROUGHT", 21);
-
-            read_RX_FIFO();
-            putchars("\n\r");
-            read_RX_FIFO();
-            putchars("\n\r");
-            read_RX_FIFO();
-            putchars("\n\r");
-            read_RX_FIFO();
-            putchars("\n\r");
-
             break;
         case SEND_INFO:
             putchar(0xAA); //send preamble to reduce the chance of the groundstation application getting a false positive on device detection
@@ -142,8 +143,14 @@ void main_loop(void) {
             putchar('S');
             #endif
             print_dec(info.frequency, 10);
-
-            //putchars("\n\r");
+            state = WAIT;
+            break;
+        case SEND_RX_BUF_STATE:
+            putchar(rx_buffer_flags);
+            putchar(get_RX_FIFO_packet_num());
+            temp = get_RX_FIFO_size();
+            putchar((char)((temp >> 8) & 0xFF));
+            putchar((char)(temp & 0xFF));
             state = WAIT;
             break;
         }
--- software/RF_Board/incl/backend.h
+++ software/RF_Board/incl/backend.h
@@ -1,7 +1,7 @@
 #ifndef BACKEND_H_
 #define BACKEND_H_
 
-enum State{INIT, WAIT, SEND_INFO};
+enum State{INIT, WAIT, SEND_INFO, SEND_RX_BUF_STATE};
 
 struct RadioInfo {
     unsigned long frequency;
@@ -12,6 +12,8 @@ unsigned char get_UART_FIFO_size(void);
 
 void write_RX_FIFO(char* data, unsigned char len);
 void read_RX_FIFO(void);
+unsigned char get_RX_FIFO_packet_num(void);
+unsigned int get_RX_FIFO_size(void);
 
 void main_loop(void);
 
